609a198b6e7be12c114dcf904e5a0b71115133be
Alex_l <leskovalesha@gmail.com>
2018-02-22 20:06:17 +0300

    commit 1

---
+++ alcoholtest.js
@@ -0,0 +1,59 @@
+﻿alert('ПРОВЕРКА НА ТРЕЗВОСТЬ!!!');
+
+while(1)
+{
+	let a = Math.floor(Math.random() * 20 - 10);
+	let b = Math.floor(Math.random() * 20 - 10);
+	let o = Math.random() * 3;
+
+	if ( Math.round(o) == 0)
+	{
+		let d = prompt ( '(' + a + ') + (' + b + ') = ');
+		if ( d == a + b )
+		{
+			alert ('Давай ещё одну!');	
+		}else
+		  {
+			alert('Харэ бухать!');
+			break;
+		  }
+
+	} else if ( Math.round(o) == 1)
+		{
+			let d = prompt ( '(' + a + ') - (' + b + ') = ');
+			if ( d == a - b ) 
+			{
+				alert ('Давай ещё одну!');	
+			}else
+		  	{
+				alert('Харэ бухать!');
+				break;
+		  	}
+
+		}else if ( Math.round(o) == 2)
+			{
+				let d = prompt ( '(' + a + ') * (' + b + ') = ');
+				if ( d == a * b ) 
+				{
+					alert ('Давай ещё одну!');	
+				}else
+		  		{
+					alert('Харэ бухать!');
+					break;
+		  		}
+
+			} else if ( Math.round(o) == 3)
+				{
+					let d = prompt ( '(' + a + ') / (' + b + ') = (с точностью до десятых) ');
+					var res = a / b;
+					if ( res.toFixed(1) == d ) 
+					{
+						alert ('Давай ещё одну!');
+					} else
+		  			{
+						alert('Харэ бухать!');
+						break;
+		  			}
+				}  
+}
+
